bba9953697eb1b34e7ac875069c7f88515368b0e
marin-h <marinahuberman@gmail.com>
2021-08-26 23:25:18 -0300

    add deploy for prod (gcp)

---
+++ deploy-prod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ echo ""
+echo "loading env vars"
+export $(grep -v '^#' .env | xargs -0)
+
+echo "feching data"
+python3 ./src/store.py
+
+echo "Deploying bot to GCR"
+echo 
+gcloud beta run deploy letras-bot --source . --set-env-vars TOKEN=${TOKEN} --allow-unauthenticated --platform managed --project $PROJECT_NAME
+
+echo "Set Telegram webhook: curl https://api.telegram.org/bot\$TOKEN/setWebHook?url=$WEBHOOK"
+curl -s "https://api.telegram.org/bot$TOKEN/setWebHook?url=$WEBHOOK" | jq .
+
+echo "Telegram Webhook Info: curl https://api.telegram.org/bot\${TOKEN}/getWebhookInfo"
+echo
+curl "https://api.telegram.org/bot${TOKEN}/getWebhookInfo" | jq .
+
