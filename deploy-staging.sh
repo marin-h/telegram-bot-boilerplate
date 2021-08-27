3f4e0f6db55249a4260ecff0576cda24d603fe6c
marin-h <marinahuberman@gmail.com>
2021-08-26 23:20:17 -0300

    fix docker run hanging staging script

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -17,7 +17,7 @@ docker build -t $CONTAINER_NAME .
 
 echo ""
 echo "running container"
-docker run --rm --env-file=".env" -e "TOKEN=${TEST_TOKEN}" -e "PORT=80" -p $PORT:80 $CONTAINER_NAME
+docker run --rm --env-file=".env" -e "TOKEN=${TEST_TOKEN}" -e "PORT=80" -p $PORT:80 $CONTAINER_NAME &
 
 echo ""
 echo "ngrok log dir: ${LOG_DIR}/ngrok.log"
