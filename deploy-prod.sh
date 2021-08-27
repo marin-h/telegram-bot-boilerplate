1ef8f8cf30ba07505d022779262199b9c2b36b20
Marina <marinahuberman@gmail.com>
2021-08-27 08:54:38 -0300

    update deploy-prod

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,12 +4,9 @@
 echo "loading env vars"
 export $(grep -v '^#' .env | xargs -0)
 
-echo "feching data"
-python3 ./src/store.py
-
 echo "Deploying bot to GCR"
 echo 
-gcloud beta run deploy letras-bot --source . --set-env-vars TOKEN=${TOKEN} --allow-unauthenticated --platform managed --project $PROJECT_NAME
+gcloud beta run deploy $PROJECT_NAME --source . --set-env-vars TOKEN=${TOKEN} --allow-unauthenticated --platform managed --project $PROJECT_NAME
 
 echo "Set Telegram webhook: curl https://api.telegram.org/bot\$TOKEN/setWebHook?url=$WEBHOOK"
 curl -s "https://api.telegram.org/bot$TOKEN/setWebHook?url=$WEBHOOK" | jq .
